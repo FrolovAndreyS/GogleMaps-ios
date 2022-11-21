4ec93d3e0dc56d9e2ffdbaa55c81c8be1c02994c
Andrey Frolov <a.s.frolov.k@gmail.com>
2022-11-21 21:44:13 +0400

    remove frameworks

--- Package.swift
+++ Package.swift
@@ -6,22 +6,25 @@ import PackageDescription
 let package = Package(
     name: "GoogleMapsPackage",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GoogleMapsPackage",
             targets: ["GoogleMapsPackage"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "GoogleMaps", path: "./Sources/GoogleMaps-ios/GoogleMaps.xcframework"),
-        .binaryTarget(name: "GoogleMapsBase", path: "./Sources/GoogleMaps-ios/GoogleMapsBase.xcframework"),
+        .binaryTarget(
+            name: "GoogleMaps",
+            url: "https://github.com/FrolovAndreyS/GogleMaps-ios/releases/download/5.0.0/GoogleMaps.xcframework.zip",
+            checksum: "117c218c2c3f96154c766bcc52909c8a631e701a63d919006c8e2f45604d8039"
+        ),
+        .binaryTarget(
+            name: "GoogleMapsBase",
+            url: "https://github.com/FrolovAndreyS/GogleMaps-ios/releases/download/5.0.0/GoogleMapsBase.xcframework.zip",
+            checksum: "6eaee4df67a68108d32ff2ccd360b8785d62f8f0fac4b8c58d2b29623aaf3a6f"
+        ),
         .target(
             name: "GoogleMapsPackage",
-            path: "Sources")
+            path: "Sources"
+        )
     ]
 )
